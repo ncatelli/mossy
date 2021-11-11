e0e0fafd74cf167a801b10a25f985a2606aa6e12
Nate Catelli <ncatelli@packetfire.org>
2021-11-11 17:18:31 +0000

    docs and codegen

--- src/pass/mod.rs
+++ src/pass/mod.rs
@@ -1,15 +1,9 @@
 pub mod type_pass;
 
+/// TreePass represents a transformation pass on the ADT. Taking an input and
+/// Output type for the pass.
 pub trait TreePass<I, O> {
     type Error;
 
     fn analyze(&mut self, input: I) -> Result<O, Self::Error>;
-}
-
-/// CodeGenerator defines the generate method, returning a string representation
-/// of all generated instructions or an error.
-pub trait CodeGenerator<I> {
-    type Error;
-
-    fn generate(&self, input: I) -> Result<Vec<String>, Self::Error>;
 }
