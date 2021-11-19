b6fb32999d6b475056efc43159e004f5ff596124
Nate Catelli <ncatelli@packetfire.org>
2021-11-19 16:41:16 -0600

    Refactor/update scrap to latest (#69)
    * Update the scrap version used
    
    * pull flag nesting out of cli
    
    * remove extra whitespace in the grammar

--- src/main.rs
+++ src/main.rs
@@ -92,21 +92,28 @@ fn main() {
     let raw_args: Vec<String> = env::args().into_iter().collect::<Vec<String>>();
     let args = raw_args.iter().map(|a| a.as_str()).collect::<Vec<&str>>();
 
+    let in_file = scrap::Flag::expect_string("in-file", "i", "an input path for a source file.");
+    let out_file = scrap::Flag::expect_string("out-file", "o", "an assembly output path.")
+        .optional()
+        .with_default("a.s".to_string());
+    let backend = scrap::Flag::with_choices(
+        "backend",
+        "b",
+        "a target architecture backend.",
+        ["x86_64".to_string()],
+        scrap::StringValue,
+    )
+    .optional()
+    .with_default("x86_64".to_string());
+
     let cmd = scrap::Cmd::new("mossy")
         .description("An (irresponsibly) experimental C compiler.")
         .author("Nate Catelli <ncatelli@packetfire.org>")
         .version("0.1.0")
-        .with_flag(scrap::Flag::expect_string(
-            "in-file",
-            "i",
-            "an input path for a source file.",
-        ))
-        .with_flag(
-            scrap::Flag::expect_string("out-file", "o", "an assembly output path.")
-                .optional()
-                .with_default("a.s".to_string()),
-        )
-        .with_handler(|(inf, ouf)| {
+        .with_flag(in_file)
+        .with_flag(out_file)
+        .with_flag(backend)
+        .with_handler(|((inf, ouf), _)| {
             read_src_file(&inf)
                 .and_then(|input| compile(&input))
                 .and_then(|asm| write_dest_file(&ouf, asm.as_bytes()))
