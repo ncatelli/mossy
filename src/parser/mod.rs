e7993251fd7a6c6569d8a71654eab330ed093df2
Nate Catelli <ncatelli@packetfire.org>
2022-02-08 18:08:55 -0600

    Escaped characters do not parse (#143)
    * fix newline_printing
    
    * add other escapes

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -878,11 +878,28 @@ numeric_type_parser!(
 );
 
 fn ascii<'a>() -> impl Parser<'a, &'a [(usize, char)], char> {
-    any_character().predicate(|c| c.is_ascii())
+    parcel::right(parcel::join(
+        expect_character('\\'),
+        expect_character('\\')
+            .map(|_| '\\')
+            .or(|| expect_character('\'').map(|_| '\''))
+            .or(|| expect_character('"').map(|_| '"')),
+    ))
+    .or(ascii_alphanumeric)
+    .or(ascii_whitespace)
+    .or(ascii_control)
+    .or(|| any_character().predicate(|c| c.is_ascii()))
 }
 
 fn ascii_whitespace<'a>() -> impl Parser<'a, &'a [(usize, char)], char> {
-    any_character().predicate(|c| c.is_ascii_whitespace())
+    parcel::right(parcel::join(
+        expect_character('\\'),
+        expect_character('n')
+            .map(|_| '\n')
+            .or(|| expect_character('t').map(|_| '\t')),
+    ))
+    .or(any_character)
+    .predicate(|c| c.is_ascii_whitespace())
 }
 
 fn ascii_alphanumeric<'a>() -> impl Parser<'a, &'a [(usize, char)], char> {
@@ -890,7 +907,11 @@ fn ascii_alphanumeric<'a>() -> impl Parser<'a, &'a [(usize, char)], char> {
 }
 
 fn ascii_control<'a>() -> impl Parser<'a, &'a [(usize, char)], char> {
-    any_character().predicate(|c| c.is_ascii_control())
+    parcel::right(parcel::join(
+        expect_character('\\'),
+        expect_character('r').map(|_| '\r'),
+    ))
+    .or(|| any_character().predicate(|c| c.is_ascii_control()))
 }
 
 fn whitespace_wrapped<'a, P, B>(parser: P) -> impl Parser<'a, &'a [(usize, char)], B>
